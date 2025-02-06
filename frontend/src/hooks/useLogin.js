65c5c47d00445244b15640663e8e86eba1317367
Roof2506 <Atticuslimtzebin@gmail.com>
2025-02-06 12:00:26 +0800

    Wohoo

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,11 +10,11 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('/api/user/login', {
+    const response = await fetch('http://localhost:4000/api/auth/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
-    })
+    });    
     const json = await response.json()
 
     if (!response.ok) {
