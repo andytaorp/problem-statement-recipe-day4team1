2bb4251ae374d6f6a68e34c9e1e0ddcff01a3633
Roof2506 <Atticuslimtzebin@gmail.com>
2025-02-06 11:39:21 +0800

    A tiny fix

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -3,18 +3,19 @@ import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const { dispatch } = useAuthContext()
 
   const signup = async (email, password) => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('/api/user/signup', {
+    const response = await fetch('http://localhost:4000/api/auth/signup', { // Use /api/auth instead of /api/user
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
-    })
+  });
+  
     const json = await response.json()
 
     if (!response.ok) {
